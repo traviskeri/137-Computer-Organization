c3d96b12fe7b6183b535d3b86e7a777d0739999e
traviskeri <traviskeri@csus.edu>
2018-04-25 16:13:14 -0700

    not working

--- behav_mealy_seq.v
+++ behav_mealy_seq.v
@@ -15,22 +15,36 @@ module output_gen(
   output z
 );
 
-assign z= q[1] & q[0] & x
+assign z= q[1] & q[0] & x;
 
 endmodule // 
 
 module flip_flop(
   input d, clk, reset,
-  output q,
+  output reg q
 );
 
-reg q;
-
-always @ ( posedge clk or negedge reset)
- 24 if (~reset) begin
- 25   q <= 1'b0;
- 26 end  else begin
- 27   q <= d;
- 28 end
+always @ ( posedge clk, posedge reset)
+begin
+    if (~reset) 
+        q <= 1'b0;
+    else 
+        q <= d;
+end
 
 endmodule //
+
+module behav_mealy_seq(
+    input clk, reset, x,
+    output z
+);
+
+wire [1:0] d;
+reg [1:0] q;
+
+    next_state_gen nsg(x, q, d);
+    flip_flop ff1(d[0], clk, reset, q[0]);
+    flip_flop ff2(d[1], clk, reset, q[1]);
+    output_gen og(q, x, z);
+
+endmodule
